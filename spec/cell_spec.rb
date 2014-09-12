60d553cbe43f8fe6bf8cdea961a16e8f698d0e0b
Bryce Kerley <bkerley@brycekerley.net>
2014-09-12 08:06:23 -0400

    add second expectation for neighbors

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,10 +3,12 @@ end
 
 describe Cell do
   subject { described_class.new(0, 0) }
+
   it 'counts neighbors' do
     expect(subject.neighbors).to eq 0
 
     c2 = described_class.new(1, 0)
+    expect(subject.neighbors).to eq 1
   end
   it 'comes to life appropriately'
   it 'stays alive appropriately'
