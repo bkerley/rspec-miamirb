4321ef7711944fa359d113e97677353551e1be02
Bryce Kerley <bkerley@brycekerley.net>
2014-09-12 07:54:51 -0400

    fill out neighbors spec

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,5 +1,10 @@
 describe 'Cell' do
-  it 'counts neighbors'
+  subject { described_class.new(0, 0) }
+  it 'counts neighbors' do
+    expect(subject.neighbors).to eq 0
+
+    c2 = described_class.new(1, 0)
+  end
   it 'comes to life appropriately'
   it 'stays alive appropriately'
 end
