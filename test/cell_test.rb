d0e7e0ca7bf4c666fc2787f28fb6193d65ce8e23
Bryce Kerley <bkerley@brycekerley.net>
2014-09-12 07:45:34 -0400

    failing neighbor count test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -2,7 +2,13 @@ require 'minitest/autorun'
 
 class CellTest < Minitest::Test
   def test_counts_neighbors
+    c = Cell.new(0, 0)
+    assert_equal 0, c.neighbors
+
+    c2 = Cell.new(1, 0)
+    assert_equal 1, c.neighbors
   end
+
   def test_comes_to_life
   end
   def test_stays_alive
