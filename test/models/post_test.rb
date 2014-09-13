c3c68296055ad228a447472694c4de44400df9fd
Bryce Kerley <bkerley@brycekerley.net>
2014-09-13 17:48:04 -0400

    failing post has many comments test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'a post has many comments' do
+    p = Post.create
+    assert_empty p.comments
+    c = Comment.create post: p
+    refute_empty p.comments
+  end
 end
